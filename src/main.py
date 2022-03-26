f3d39b43157677c788c88e0ea48a36d746270452
MiguelAMM42 <a93280@alunos.uminho.pt>
2022-03-26 13:47:50 +0000

    All the HTML are created with sucess; Need to improve some visual aspect and test some things

--- src/main.py
+++ src/main.py
@@ -1,30 +1,11 @@
 import sys
 from reader import reader
 from queries import *
-from athletesGenerator import *
 from htmlGenerator import *
 
 def main(csvFile):
-    content = reader(csvFile)
-    #print(content)
-
-    dist = distByYearAndSport(content)
-    print(dist)
-
-
-    #print(content)
-    #athletesGenerator(content)
-    #dist = distByAddress(content)
-    #print(dist)
-    #distByAddressHTML(dist)
-
-    #dist2 = distByDates(content) 
-    #distByDatesHTML(dist2)
-
-    #dist3 = distByYearAndGender(content)
-    #distByYearAndGender(dist3)
-
-    #htmlMAIN()
+    athletes = reader(csvFile)
+    HTMLsGenerator(athletes)
     
     
 
